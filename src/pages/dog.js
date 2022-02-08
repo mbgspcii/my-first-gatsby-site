c61366c16ec0758cc2461b1b91d8a3f1ed703684
Miguel Beignon <miguel@twic.pics>
2022-02-08 12:32:41 +0100

    why ssr ko ???

--- src/pages/dog.js
+++ src/pages/dog.js
@@ -1,25 +1,7 @@
 import * as React from "react"
-const DogPage = ({ serverData }) => (
+const DogPage = () => (
   <main>
     <h1>SSR Page with Dogs</h1>
-    <img alt="Happy dog" src={serverData.message} />
-  </main>
+    </main>
 )
 export default DogPage
-export async function getServerData() {
-  try {
-    const res = await fetch(`https://dog.ceo/api/breeds/image/random`)
-    if (!res.ok) {
-      throw new Error(`Response failed`)
-    }
-    return {
-      props: await res.json(),
-    }
-  } catch (error) {
-    return {
-      status: 500,
-      headers: {},
-      props: {}
-    }
-  }
-}
