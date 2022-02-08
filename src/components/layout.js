c512db55ee2f3d2895f43789e7d74a9b031705af
Miguel Beignon <miguel@twic.pics>
2022-02-08 13:07:46 +0100

    why ssr ko 4 ???

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,6 +19,7 @@ const Layout = ( { pageTitle, children } ) => {
                     <li className={navLinkItem}><Link to='/dog' className={navLinkText}>Dog ssr</Link></li>
                     <li className={navLinkItem}><Link to='/about' className={navLinkText}>About</Link></li>
                     <li className={navLinkItem}><Link to='/aboutv2' className={navLinkText}>About V2</Link></li>
+                    <li className={navLinkItem}><Link to='/cat' className={navLinkText}>Cat</Link></li>
                 </ul>
             </nav>
             <main>
---
+++ src/pages/cat.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+const CatPage = () => {
+    return ( 
+        <Layout pageTitle='Cat Page'>
+            <p>Hi there! </p>
+        </Layout>
+
+    );
+}
+
+export default CatPage;
